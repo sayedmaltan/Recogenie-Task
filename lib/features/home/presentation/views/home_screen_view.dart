cf796918a3aca36af2e966118e47d8904be03607
Elsayed Ahmed <93007715+sayedmaltan@users.noreply.github.com>
2025-07-26 16:24:56 +0300

    setup firebase auth gate

--- lib/features/home/presentation/views/home_screen_view.dart
+++ lib/features/home/presentation/views/home_screen_view.dart
@@ -5,6 +5,8 @@ class HomeScreenView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Column(),
+    );
   }
 }
